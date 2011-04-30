035e50d8fbbcf1480c659dbabfb74ab8201af973
Greg Peterson <gpeterson2@gmail.com>
2011-04-29 20:46:03 -0500

    Initially adding

---
+++ main.py
@@ -0,0 +1,54 @@
+#! /usr/bin/env python
+
+import csv
+import re
+import sys
+
+def read_file(infile, outfile):
+    text = open(infile, 'r').read()
+
+    converter = Converter(output=outfile)
+    for line in text.split('\n'):
+        converter.convert(line)
+
+class Converter(object):
+    def __init__(self, output, error=None):
+        if not error:
+            error = sys.stderr
+
+        self.output = output
+        self.error = error
+
+    def convert(self, line):
+        if not line:
+            return
+
+        pattern = r'(.*)\s*\xe3\x80\x90(.*)\xe3\x80\x91\s*(.*)'
+        regex = re.compile(pattern, re.U)
+
+        try:
+            match = regex.match(line).groups()
+
+            kanji = match[0].strip()
+            kana = match[1].strip()
+            reading = match[2].strip()
+
+            front = kanji
+            back = '{0}<br>{1}'.format(kana, reading)
+
+            self.output.writerow([front, back])
+
+        except AttributeError as e:
+            self.error.write('{0}\n'.format(line))
+
+if __name__ == '__main__':
+    args = sys.argv 
+
+    if len(args) != 2:
+        print('Usage main.py input')
+        sys.exit(1)
+
+    infile = args[1]
+    outfile = csv.writer(open('blah.csv', 'w'))
+    read_file(infile, outfile)
+    
